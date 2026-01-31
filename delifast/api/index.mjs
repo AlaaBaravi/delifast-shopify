ace3ba1d1718971549351c2a41fdefc7458e480f
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-01-31 23:38:18 +0200

    Update index.mjs

--- delifast/api/index.mjs
+++ delifast/api/index.mjs
@@ -2,7 +2,11 @@ import { createRequestHandler } from "@vercel/node";
 
 // IMPORTANT: this path is to the built server bundle (generated during build)
 import * as build from "../build/server/index.js";
+export default function handler(req, res) {
+  res.status(200).send("API WORKS ✅");
+}
 
 export default createRequestHandler({
   build,
 });
+
