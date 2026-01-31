970272d2fa7a7baf8197f906dc332432174cbc25
AlaaBaravi <97338784+AlaaBaravi@users.noreply.github.com>
2026-01-31 22:47:41 +0200

    Update    index.mjs

--- delifast/api/   index.mjs
+++ delifast/api/   index.mjs
@@ -1,4 +1,14 @@
 import { createRequestHandler } from "@react-router/node";
 import * as build from "../build/server/index.js";
 
-export default createRequestHandler(build, process.env.NODE_ENV);
+const handler = createRequestHandler(build, process.env.NODE_ENV);
+
+export default async function (req, res) {
+  try {
+    return await handler(req, res);
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end("Server error");
+  }
+}
